a0d1c694db54762c00033797c733884f006bf655
yerkebulan <yerkebulanyessenali03@gmail.com>
2024-08-13 06:33:45 +0500

    init project and add configs

---
+++ src/main/java/com/example/data_analyzer_microservice/DataAnalyzerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_analyzer_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyzerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalyzerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/data_analyzer_microservice/DataAnalyzerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data_analyzer_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyzerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
