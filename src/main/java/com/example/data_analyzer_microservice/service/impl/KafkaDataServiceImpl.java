8917976e27b17180d34dbe86c86a5a1b35b522b7
yerkebulan <yerkebulanyessenali03@gmail.com>
2024-08-15 06:15:49 +0500

    some refactoring and test

--- src/main/java/com/example/data_analyzer_microservice/service/impl/KafkaDataServiceImpl.java
+++ src/main/java/com/example/data_analyzer_microservice/service/impl/KafkaDataServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.data_analyzer_microservice.service.impl;
 
 import com.example.data_analyzer_microservice.model.Data;
 import com.example.data_analyzer_microservice.service.KafkaDataService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaDataServiceImpl implements KafkaDataService {
     @Override
     public void handle(Data data) {
