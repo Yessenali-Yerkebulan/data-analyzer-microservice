799c1fe4084c297339c6703055c9e5c58c5decdc
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-09-07 15:58:30 +0500

    save data to database

--- src/main/java/com/example/data_analyzer_microservice/service/impl/KafkaDataServiceImpl.java
+++ src/main/java/com/example/data_analyzer_microservice/service/impl/KafkaDataServiceImpl.java
@@ -1,13 +1,24 @@
 package com.example.data_analyzer_microservice.service.impl;
 
 import com.example.data_analyzer_microservice.model.Data;
+import com.example.data_analyzer_microservice.repository.DataRepository;
 import com.example.data_analyzer_microservice.service.KafkaDataService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class KafkaDataServiceImpl implements KafkaDataService {
+	
+	private final DataRepository dataRepository;
+	
     @Override
     public void handle(Data data) {
-        System.out.println("Data object is received: " + data.toString());
+        log.info("Data object {} was saved", data);
+        dataRepository.save(data);
     }
 }
