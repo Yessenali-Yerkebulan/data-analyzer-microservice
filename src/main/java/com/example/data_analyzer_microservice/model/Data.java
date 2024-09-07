8b9c8147db58640f79ae2d1d96430040202c58ab
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-09-07 15:36:34 +0500

    define Data entity

--- src/main/java/com/example/data_analyzer_microservice/model/Data.java
+++ src/main/java/com/example/data_analyzer_microservice/model/Data.java
@@ -7,14 +7,31 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "data")
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Data {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
     private Long sensorId;
     private LocalDateTime timestamp;
     private double measurement;
+    
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
     private MeasurementType measurementType;
 
     public enum MeasurementType {
